cf982ae7cd8779f8837469f84c7359e672427519
Drake Rubicon <drake@502c49c6-b994-456f-ad61-551bcbba8fd3.fritz.box>
2026-01-09 00:27:31 +0100

    Added further renaming to Job-Script, refactored artboard functions

--- Illustrator/active/Filme-YYYY-MM-DD-Jobinfos.jsx
+++ Illustrator/active/Filme-YYYY-MM-DD-Jobinfos.jsx
@@ -24,65 +24,96 @@
 
     // ---------- OBJECT COUNT FUNCTION ----------
     function countTopLayerObjectsOnArtboard(doc, artboardIndex) {
-    var ab = doc.artboards[artboardIndex];
-    var abRect = ab.artboardRect; // [left, top, right, bottom]
-    var count = 0;
+        var ab = doc.artboards[artboardIndex];
+        var abRect = ab.artboardRect; // [left, top, right, bottom]
+        var count = 0;
 
-    for (var l = 0; l < doc.layers.length; l++) {
-        var layer = doc.layers[l];
+        for (var l = 0; l < doc.layers.length; l++) {
+            var layer = doc.layers[l];
 
-        if (!layer.visible || layer.locked) continue;
+            if (!layer.visible || layer.locked) continue;
 
-        for (var i = 0; i < layer.pageItems.length; i++) {
-            var item = layer.pageItems[i];
+            for (var i = 0; i < layer.pageItems.length; i++) {
+                var item = layer.pageItems[i];
 
-            // Only top-level items of the layer
-            if (item.parent !== layer) continue;
-            if (item.hidden || item.locked) continue;
+                // Only top-level items of the layer
+                if (item.parent !== layer) continue;
+                if (item.hidden || item.locked) continue;
 
-            try {
-                var b = item.visibleBounds;
+                try {
+                    var b = item.visibleBounds;
 
-                if (
-                    b[2] > abRect[0] &&
-                    b[0] < abRect[2] &&
-                    b[1] > abRect[3] &&
-                    b[3] < abRect[1]
-                ) {
-                    count++;
-                }
-            } catch (e) {}
+                    if (
+                        b[2] > abRect[0] &&
+                        b[0] < abRect[2] &&
+                        b[1] > abRect[3] &&
+                        b[3] < abRect[1]
+                    ) {
+                        count++;
+                    }
+                } catch (e) { }
+            }
         }
+
+        return count;
     }
 
-    return count;
-}
+    // ---------- COLLECT ARTBOARD INFO FUNCTION ----------
+    function collectArtboardInfo(doc, ptToMm) {
+        var info = [];
+        var abCount = doc.artboards.length;
 
+        for (var i = 0; i < abCount; i++) {
+            var ab = doc.artboards[i];
+            var r = ab.artboardRect;
 
-    // ---------- ARTBOARD DATA ----------
-    var abCount = doc.artboards.length;
+            var width = Math.round((r[2] - r[0]) * ptToMm);
+            var height = Math.round((r[1] - r[3]) * ptToMm);
+
+            var objCount = countTopLayerObjectsOnArtboard(doc, i);
+            var spurWord = (objCount === 1) ? "Spur" : "Spuren";
+
+            info.push({
+                index: i,
+                width: width,
+                height: height,
+                objCount: objCount,
+                spurWord: spurWord
+            });
+        }
+
+        return info;
+    }
+
+    // ---------- COLLECT ARTBOARD INFO ----------
+    var abInfo = collectArtboardInfo(doc, ptToMm);
+    var abCount = abInfo.length;
+
+    // ---------- RENAME ARTBOARDS ----------
+    for (var i = 0; i < abCount; i++) {
+        var a = abInfo[i];
+        doc.artboards[i].name =
+            "Film " + (i + 1) + ", " +
+            a.width + "x" + a.height + "mm, " +
+            a.objCount + " " + a.spurWord;
+    }
+
+    // ---------- BUILD FILENAME ----------
+    var filmWord = (abCount === 1) ? "Film" : "Filme";
     var abData = [];
 
     for (var i = 0; i < abCount; i++) {
-        var ab = doc.artboards[i];
-        var r = ab.artboardRect;
+        var a = abInfo[i];
 
-        var width = Math.round((r[2] - r[0]) * ptToMm);
-        var height = Math.round((r[1] - r[3]) * ptToMm);
-
-        var objCount = countTopLayerObjectsOnArtboard(doc, i);
-
-        abData.push("{" + width + "x" + height + "mm, " + objCount + " Spuren}");
+        if (abCount === 1) {
+            abData.push(a.width + "x" + a.height + "mm, " + a.objCount + " " + a.spurWord);
+        } else {
+            abData.push("{" + a.width + "x" + a.height + "mm, " + a.objCount + " " + a.spurWord + "}");
+        }
     }
 
-    // ---------- FILENAME ----------
     var newFileName =
-        dateStr +
-        "_" +
-        abCount +
-        " Filme_[" +
-        abData.join(",") +
-        "].ai";
+            dateStr + "_" + abCount + " " + filmWord + "_[" + abData.join(",") + "].ai";
 
     // ---------- SAVE LOCATION ----------
     var folder;
